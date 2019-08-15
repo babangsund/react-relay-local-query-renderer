a082bd7685365833b39529fc925e0b3937f20305
babangsund <babangsund@gmail.com>
2019-08-15 21:26:51 +0200

    mangle esm

--- rollup.config.js
+++ rollup.config.js
@@ -5,48 +5,49 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 
 import pkg from './package.json';
 
-const base = {
-  input: 'src/index.js',
-  external: Object.keys(pkg.dependencies)
+const input = 'src/index.js';
+const external = ['@babel/runtime'];
+const license = {
+  output: {
+    preamble: [
+      '/**',
+      ' * react-relay-local-query-renderer v' + process.env.npm_package_version,
+      ' *',
+      ' * Copyright (c) 2019 babangsund',
+      ' *',
+      ' * This source code is licensed under the MIT license found in the',
+      ' * LICENSE file in the root directory of this source tree.',
+      ' */'
+    ].join('\n')
+  }
 };
 
-function makePlugins(minify) {
-  return [
+export default {
+  input,
+  external,
+  plugins: [
     babel({
       runtimeHelpers: true,
-      exclude: 'node_modules/**'
+      exclude: 'node_modules/**',
+      plugins: [['@babel/transform-runtime', { useESModules: true }]]
     }),
     nodeResolve(),
     commonjs(),
-    minify && terser()
-  ];
-}
-const esm = {
-  ...base,
-  output: {
-    format: 'esm',
-    file: pkg.module
-  },
-  plugins: makePlugins(false)
+    process.env.NODE_ENV === 'production' && terser(license)
+  ],
+  output: [
+    {
+      format: 'esm',
+      file: pkg.module
+    },
+    {
+      format: 'cjs',
+      file: pkg.main
+    },
+    {
+      format: 'umd',
+      name: 'ReactRelayLocalQueryRenderer',
+      file: `dist/index.umd.${process.env.NODE_ENV}.js`
+    }
+  ]
 };
-
-const cjs = {
-  ...base,
-  output: {
-    format: 'cjs',
-    file: pkg.main
-  },
-  plugins: makePlugins(true)
-};
-
-const umd = {
-  ...base,
-  output: {
-    format: 'umd',
-    name: 'ReactRelayLocalQueryRenderer',
-    file: `dist/index.umd.${process.env.NODE_ENV}.js`
-  },
-  plugins: makePlugins(process.env.NODE_ENV === 'production')
-};
-
-export default [umd, cjs, esm];
